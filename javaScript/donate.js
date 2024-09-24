29897785c66e4d6db9e16ec4ad28dc98200dfe06
rubayat-rafi <rubayatrafi522@gmail.com>
2024-09-25 02:30:41 +0600

    use common function

--- javaScript/donate.js
+++ javaScript/donate.js
@@ -1,229 +1,118 @@
 const donation = document.getElementById('donation-card');
 const history = document.getElementById('history-card');
+
 const noakhali = document.getElementById('input-box1');
 const feni = document.getElementById('input-box2');
 const quota = document.getElementById('input-box3');
+
 const noakhaliBtn = document.getElementById('noakhali-btn');
 const feniBtn = document.getElementById('feni-btn');
 const quotaBtn = document.getElementById('quota-btn');
-const mainBalance = document.getElementById('main-balance');
+
 const noakhaliDonationBdt = document.getElementById('noakhali-donation-bdt');
 const feniDonationBdt = document.getElementById('feni-donation-bdt');
 const quotaDonationBdt = document.getElementById('quota-donation-bdt');
-const historySection = document.getElementById('history-section');
+
+const mainBalance = document.getElementById('main-balance');
+
 const noDonation = document.getElementById('no-donation');
 
+const historySection = document.getElementById('history-section');
 
 
+// common function 
+function commonDonation(donationBalanceBdt, input, place ){
+    
+    const totalMainBalance = parseFloat(mainBalance.innerText)
+    const donationBalance = parseFloat(donationBalanceBdt.innerText);
+    const donationAmount = parseFloat(input.value);
+
+    if(isNaN(donationAmount) || donationAmount <= 0){
+       alert('Invalid number. Please enter a valid positive number.');
+       return;
+    }
+    if(donationAmount > totalMainBalance){
+       alert('Insufficient balance!');
+       return;
+    }
+
+       const totalBalance = donationBalance + donationAmount;
+        donationBalanceBdt.innerText = totalBalance.toFixed(2);
+   
+       const accountBalance = totalMainBalance - donationAmount;
+       mainBalance.innerText = accountBalance.toFixed(2);
+
+   // history section
+        noDonation.classList.add('hidden');
+
+        const h = document.createElement('div');
+           h.classList.add('w-full');
+           h.innerHTML = ` 
+           <div  class="p-5 md:p-8 border rounded-2xl w-full">
+               <h3 class="text-base md:text-xl font-bold text-textColor mb-4 leading-8">
+
+               ${donationAmount} Taka is Donated for  ${place}, Bangladesh </h3>
+
+               <p class="text-sm md:text-base font-light  text-textColor text-opacity-70 leading-7">
+
+               Date : ${new Date()}</p>
+            </div>
+   `;
+       historySection.appendChild(h);
+
+       // show the dialog 
+       my_modal_5.showModal();
+};
 
 // noakhali donation box 
-noakhali.addEventListener('keyup', function(event){
-    const number = parseFloat(event.target.value);
-
-    if(!isNaN(number) &&  number > 0){
-        noakhaliBtn.removeAttribute('disabled');
-    }else{
-        noakhaliBtn.setAttribute('disabled', ture);
-    }
-})
-
 noakhaliBtn.addEventListener('click', function(event){
     event.preventDefault();
 
-     const totalMainBalance = parseFloat(mainBalance.innerText)
-     const donationBalance = parseFloat(noakhaliDonationBdt.innerText);
-     const donationAmount = parseFloat(noakhali.value);
-
-     if(isNaN(donationAmount) || donationAmount <= 0){
-        alert('Invalid number. Please enter a valid positive number.');
-        return;
-     }
-     if(donationAmount > totalMainBalance){
-        alert('Insufficient balance!');
-        return;
-     }
-
-
-        const totalBalance = donationBalance + donationAmount;
-        document.getElementById('noakhali-donation-bdt').innerText = totalBalance.toFixed(2);
-    
-        const accountBalance = totalMainBalance - donationAmount;
-        document.getElementById('main-balance').innerText = accountBalance.toFixed(2);
-
-    // history section
-    noDonation.classList.add('hidden');
-     
-    const h = document.createElement('div');
-            h.classList.add('w-full');
-            h.innerHTML = ` 
-            <div  class="p-8 border rounded-2xl w-full">
-                <h3 class="text-xl font-bold text-textColor mb-4 leading-8">
-
-                ${donationAmount} Taka is Donated for famine-2024 at Noakhali, Bangladesh </h3>
-
-                <p class="text-base font-light  text-textColor text-opacity-70 leading-7 mb-6">
-
-                Date :${new Date()}</p>
-             </div>
-    `
-        historySection.appendChild(h);
-
-        // show the dialog 
-        my_modal_5.showModal();
+    commonDonation( noakhaliDonationBdt, noakhali, 'famine-2024 at Noakhali');
 });
 
-
 // feni donation box 
-feni.addEventListener('keyup', function(event){
-    const number = parseFloat(event.target.value);
-
-    if(!isNaN(number) &&  number > 0){
-        feniBtn.removeAttribute('disabled');
-    }else{
-        feniBtn.setAttribute('disabled', ture);
-    }
-})
-
 feniBtn.addEventListener('click', function(event){
     event.preventDefault();
 
-     const totalMainBalance = parseFloat(mainBalance.innerText)
-     const donationBalance = parseFloat(feniDonationBdt.innerText);
-     const donationAmount = parseFloat(feni.value);
-
-     if(isNaN(donationAmount) || donationAmount <= 0){
-        alert('Invalid number. Please enter a valid positive number.');
-        return;
-     }
-     if(donationAmount > totalMainBalance){
-        alert('Insufficient balance!');
-        return;
-     }
-
-        const totalBalance = donationBalance + donationAmount;
-        document.getElementById('feni-donation-bdt').innerText = totalBalance.toFixed(2);
-    
-        const accountBalance = totalMainBalance - donationAmount;
-        document.getElementById('main-balance').innerText = accountBalance.toFixed(2);
-
-    // history section
-    noDonation.classList.add('hidden');
-
-    const h = document.createElement('div');
-            h.classList.add('w-full');
-            h.innerHTML = ` 
-            <div  class="p-8 border rounded-2xl w-full">
-                <h3 class="text-xl font-bold text-textColor mb-4 leading-8">
-
-                ${donationAmount} Taka is Donated for famine-2024 at Feni, Bangladesh </h3>
-
-                <p class="text-base font-light  text-textColor text-opacity-70 leading-7 mb-6">
-
-                Date :${new Date()}</p>
-             </div>
-    `
-        historySection.appendChild(h);
-
-        // show the dialog 
-        my_modal_5.showModal();
+    commonDonation(feniDonationBdt, feni, 'famine-2024 at feni');
 });
-
-
-
-
-
 
 // quota donation box 
-quota.addEventListener('keyup', function(event){
-    const number = parseFloat(event.target.value);
-
-    if(!isNaN(number) &&  number > 0){
-        quotaBtn.removeAttribute('disabled');
-    }else{
-        quotaBtn.setAttribute('disabled', ture);
-    }
-})
-
 quotaBtn.addEventListener('click', function(event){
     event.preventDefault();
-
-     const totalMainBalance = parseFloat(mainBalance.innerText)
-     const donationBalance = parseFloat(quotaDonationBdt.innerText);
-     const donationAmount = parseFloat(quota.value);
-
-     if(isNaN(donationAmount) || donationAmount <= 0){
-        alert('Invalid number. Please enter a valid positive number.');
-        return;
-     }
-     if(donationAmount > totalMainBalance){
-        alert('Insufficient balance!');
-        return;
-     }
-
-        const totalBalance = donationBalance + donationAmount;
-        document.getElementById('quota-donation-bdt').innerText = totalBalance.toFixed(2);
     
-        const accountBalance = totalMainBalance - donationAmount;
-        document.getElementById('main-balance').innerText = accountBalance.toFixed(2);
-
-    // history section
-    noDonation.classList.add('hidden');
-    
-    const h = document.createElement('div');
-            h.classList.add('w-full');
-            h.innerHTML = ` 
-            <div  class="p-8 border rounded-2xl w-full">
-                <h3 class="text-xl font-bold text-textColor mb-4 leading-8">
-
-                ${donationAmount} Taka is Donated for Aid for Injured in the Quota Movement </h3>
-
-                <p class="text-base font-light  text-textColor text-opacity-70 leading-7 mb-6">
-
-                Date :${new Date()}</p>
-             </div>
-    `
-        historySection.appendChild(h);
-
-        // show the dialog 
-        my_modal_5.showModal();
+    commonDonation(quotaDonationBdt, quota, 'Aid for Injured in the Quota Movement');
 });
 
 
 
 
 
-// Donation button 
+// common function for toggle button
+function toggleSection(hide , show , activeBtn , inactiveBtn){
+
+    hide.classList.add('hidden');
+    show.classList.remove('hidden');
+
+    // active button color 
+    activeBtn.style.backgroundColor='#B4F461';
+    activeBtn.style.border='2px solid #B4F461';
+    activeBtn.style.color='#111111';
+
+    // inactive button color 
+    inactiveBtn.style.backgroundColor='transparent';
+    inactiveBtn.style.color='rgba(17, 17, 17, 0.7)';
+    inactiveBtn.style.border=' 2px solid rgba(17, 17, 17, 0.3)';
+}
+
 document.getElementById('donation-btn').addEventListener('click', function(){
-    history.classList.add('hidden')
-    donation.classList.remove('hidden')
-
-    // donation button color 
-    document.getElementById('donation-btn').style.backgroundColor='#B4F461';
-    document.getElementById('donation-btn').style.border='2px solid #B4F461';
-    document.getElementById('donation-btn').style.color='#111111';
-
-
-     // history btn color 
-     document.getElementById('history-btn').style.backgroundColor='transparent';
-     document.getElementById('history-btn').style.color='rgba(17, 17, 17, 0.7)';
-     document.getElementById('history-btn').style.border=' 2px solid rgba(17, 17, 17, 0.3)';
+    toggleSection(history , donation , document.getElementById('donation-btn') ,
+                    document.getElementById('history-btn'));
 })
 
-// history button 
 document.getElementById('history-btn').addEventListener('click', function(){
-    donation.classList.add('hidden')
-    history.classList.remove('hidden')
-
-    // history btn color 
-    document.getElementById('history-btn').style.backgroundColor='#B4F461';
-    document.getElementById('history-btn').style.border='2px solid #B4F461';
-    document.getElementById('history-btn').style.color='#111111';
-
-    // donation button color 
-    document.getElementById('donation-btn').style.backgroundColor='transparent';
-    document.getElementById('donation-btn').style.color='rgba(17, 17, 17, 0.7)';
-    document.getElementById('donation-btn').style.border=' 2px solid rgba(17, 17, 17, 0.3)';
-
+    toggleSection(donation, history, document.getElementById('history-btn'),
+    document.getElementById('donation-btn'));
 })
-
 
